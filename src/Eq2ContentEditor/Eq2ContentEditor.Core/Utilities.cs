ad4b95c8d672ce0a849f2eb31f42b82286c66648
PaulDevenney <paul.devenney@googlemail.com>
2016-10-06 23:40:05 +0100

    Added tests demo

---
+++ src/Eq2ContentEditor/Eq2ContentEditor.Core/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eq2ContentEditor.Core
+{
+    public class Utilities
+    {
+        public int CalcSpellBuffValue(int level, int potency)
+        {
+            var result = level * potency;
+            //if (result > 100)
+            //{
+            //    result = 100;
+            //}
+
+            return result;
+        }
+    }
+}
---
+++ src/Eq2ContentEditor/Eq2ContentEditor.Tests/UtilitiesTests.cs
@@ -0,0 +1,19 @@
+﻿using Eq2ContentEditor.Core;
+using Xunit;
+
+namespace Eq2ContentEditor.Tests
+{
+    public class UtilitiesTests
+    {
+        
+        [Fact]
+        public void SpellBuff_CannotBeGreaterThan100()
+        {
+            var sut = new Utilities();
+
+            var actual = sut.CalcSpellBuffValue(100, 5);
+
+            Assert.True(actual <= 100, "Spell Buff value cannot be greated than 100");
+        }
+    }
+}
