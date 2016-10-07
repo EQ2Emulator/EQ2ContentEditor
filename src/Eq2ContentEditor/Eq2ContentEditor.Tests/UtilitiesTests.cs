217199f847329c3bb39799c02a5cc9be5c7b1829
PaulDevenney <paul.devenney@googlemail.com>
2016-10-07 14:07:34 +0100

    improved test demo

--- src/Eq2ContentEditor/Eq2ContentEditor.Tests/UtilitiesTests.cs
+++ src/Eq2ContentEditor/Eq2ContentEditor.Tests/UtilitiesTests.cs
@@ -6,10 +6,15 @@ namespace Eq2ContentEditor.Tests
     [Trait("Category", "Spells")]
     public class UtilitiesTests
     {
-        
+        [Theory(DisplayName = "Spell Buff Cannot be Greater Than 100")]
+        [InlineData(100,5)]
+        [InlineData(1, 8)]
+        [InlineData(1, -8)]
+        public void SpellBuff_CannotBeGreaterThan100(int lvl, int multiplier)
         {
             var sut = new Utilities();
 
+            var actual = sut.CalcSpellBuffValue(lvl, multiplier);
 
             Assert.True(actual <= 100, "Spell Buff value cannot be greated than 100");
         }
